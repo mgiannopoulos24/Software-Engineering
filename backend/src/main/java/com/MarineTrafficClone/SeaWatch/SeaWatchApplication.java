517f7f0620250340b6c1cbea9f07b4ac4060a9ce
George Korillos <george.korillos@gmail.com>
2025-06-18 17:06:37 +0300

    redistribute

---
+++ backend/src/main/java/com/MarineTrafficClone/SeaWatch/SeaWatchApplication.java
@@ -0,0 +1,13 @@
+package com.MarineTrafficClone.SeaWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaWatchApplication.class, args);
+	}
+
+}
