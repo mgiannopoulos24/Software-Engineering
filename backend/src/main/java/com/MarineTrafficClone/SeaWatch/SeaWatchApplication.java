18e74d8c0a51061db77761eb3a9e924832779f14
George Korillos <george.korillos@gmail.com>
2025-06-30 01:39:25 +0300

    SSL added

--- backend/src/main/java/com/MarineTrafficClone/SeaWatch/SeaWatchApplication.java
+++ backend/src/main/java/com/MarineTrafficClone/SeaWatch/SeaWatchApplication.java
@@ -3,9 +3,15 @@ package com.MarineTrafficClone.SeaWatch;
 import com.MarineTrafficClone.SeaWatch.enumeration.RoleType;
 import com.MarineTrafficClone.SeaWatch.model.User;
 import com.MarineTrafficClone.SeaWatch.repository.UserRepository;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +22,31 @@ public class SeaWatchApplication {
 		SpringApplication.run(SeaWatchApplication.class, args);
 	}
 
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 
     @Bean
     CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
