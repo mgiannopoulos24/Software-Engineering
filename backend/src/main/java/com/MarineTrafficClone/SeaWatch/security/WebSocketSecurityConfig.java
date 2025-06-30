e5880c9af2c97ee699d519137ca03499be4d1b42
George Korillos <george.korillos@gmail.com>
2025-06-30 09:08:02 +0300

    Moved WebSocketSecurityConfig

--- backend/src/main/java/com/MarineTrafficClone/SeaWatch/security/WebSocketSecurityConfig.java
+++ backend/src/main/java/com/MarineTrafficClone/SeaWatch/security/WebSocketSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.MarineTrafficClone.SeaWatch.configuration;
+package com.MarineTrafficClone.SeaWatch.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
