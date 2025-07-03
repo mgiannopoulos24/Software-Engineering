7f9fed140ccdff143ced27992cbcf05aaf29b323
George Korillos <george.korillos@gmail.com>
2025-07-03 05:29:06 +0300

    Disabled HTTPS and Allowed more Origins in WebSocketConfig for testing Websockets

--- backend/src/main/java/com/MarineTrafficClone/SeaWatch/SeaWatchApplication.java
+++ backend/src/main/java/com/MarineTrafficClone/SeaWatch/SeaWatchApplication.java
@@ -24,31 +24,31 @@ public class SeaWatchApplication {
 		SpringApplication.run(SeaWatchApplication.class, args);
 	}
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory    tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory    tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector redirectConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8080);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8443);
+//        return connector;
+//    }
 
     @Bean
     CommandLineRunner initDatabase(UserEntityRepository userEntityRepository, PasswordEncoder passwordEncoder) {
--- backend/src/main/java/com/MarineTrafficClone/SeaWatch/configuration/WebSocketConfig.java
+++ backend/src/main/java/com/MarineTrafficClone/SeaWatch/configuration/WebSocketConfig.java
@@ -26,6 +26,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-ais").setAllowedOrigins("https://localhost:5173").withSockJS();
+        registry.addEndpoint("/ws-ais").setAllowedOrigins("https://localhost:5173", "http://localhost:5173", "http://localhost:8080").withSockJS();
     }
 }
