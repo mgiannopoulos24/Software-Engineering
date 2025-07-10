989b267d524e2afd6bc956a16b82b65bb36aa244
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-07-10 20:42:03 +0300

    more tests

--- frontend/src/tests/test-utils.tsx
+++ frontend/src/tests/test-utils.tsx
@@ -1,25 +1,39 @@
 import { AuthProvider } from '@/contexts/AuthContext';
+import { FleetProvider } from '@/contexts/FleetContext';
 import { NotificationProvider } from '@/contexts/NotificationContext';
-import { RenderOptions, render } from '@testing-library/react';
+import { WebSocketProvider } from '@/contexts/WebSocketContext';
+import { ZoneProvider } from '@/contexts/ZoneContext';
+import { render, RenderOptions } from '@testing-library/react';
 import React, { ReactElement } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
+// Ένας wrapper που περιλαμβάνει ΟΛΟΥΣ τους providers που χρειάζονται τα components μας
 const AllTheProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <BrowserRouter>
       <AuthProvider>
         <NotificationProvider>
-          {children}
-          <Toaster />
+          <WebSocketProvider>
+            <FleetProvider>
+              <ZoneProvider>
+                {children}
+                <Toaster />
+              </ZoneProvider>
+            </FleetProvider>
+          </WebSocketProvider>
         </NotificationProvider>
       </AuthProvider>
     </BrowserRouter>
   );
 };
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: AllTheProviders, ...options });
+// Custom render function που χρησιμοποιεί το wrapper μας
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllTheProviders, ...options });
 
+// Εξάγουμε ξανά τα πάντα από το @testing-library/react, αλλά αντικαθιστούμε το render με το δικό μας
 export * from '@testing-library/react';
 export { customRender as render };
